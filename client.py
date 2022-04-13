82470cbc2c9195876a45f1307e30a4e14c3e9ba0
Shannen Geller <shannengeller7@gmail.com>
2022-04-13 12:46:42 -0400

    updated

--- client.py
+++ client.py
@@ -9,4 +9,35 @@ s = socket.socket()
 socket_host = socket.gethostname()
 ip_addr = socket.gethostbyname(socket_host)
 
-#connect
+#getting host name and address from user
+host = input(str("What is the host address you want to use?"))
+name = input(str("What is your name? "))
+
+#port number
+port = 14500
+
+#printing to user so they can see where they are at
+print("Connecting to " + host + "...")
+
+#connecting to socket
+s.connect((host, port))
+print("connected to " + host + "!")
+
+#sending message to server
+s.send(name.encode())
+server_name = s.recv(1024).decode()
+print("Connected to: " + server_name)
+
+#while loop to keep program running as long as the user doesn't exit
+while True:
+    #prompting the user to send a message
+    message = s.recv(1024).decode()
+    print(server_name + ": " + message)
+    message = input(str("Me: (HiEnter a message to send):"))
+    # if the user hits [E]/[e] then the program will exit
+    if message == "e" or message == "E":
+        print("Exiting...")
+        break
+    #else sending message to server
+    else:
+        s.send(message.encode())
